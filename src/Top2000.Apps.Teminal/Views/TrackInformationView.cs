0bfdcfe35af8d4582871b58fb744273fd69370d6
Rick Neeft <rick.neeft@outlook.com>
2024-09-15 16:39:50 +0200

    All info visible

--- src/Top2000.Apps.Teminal/Views/TrackInformationView.cs
+++ src/Top2000.Apps.Teminal/Views/TrackInformationView.cs
@@ -22,35 +22,194 @@ public class TrackInformationView : View
     {
         var trackInformation = await this.mediator.Send(new TrackInformationRequest { TrackId = trackId });
 
-        this.Add(new Label()
+        var title = new Label()
         {
             X = 0,
             Y = 0,
             Height = 1,
             Width = Dim.Fill(),
             Text = trackInformation.Title,
+        };
+
+
+        var artist = new Label
+        {
+            X = 0,
+            Y = Pos.Bottom(title),
+            Height = 1,
+            Width = trackInformation.Artist.Length,
+            Text = trackInformation.Artist,
+        };
+
+        var recordingYear = new Label
+        {
+            X = Pos.Right(artist) + 1,
+            Y = Pos.Bottom(title),
+            Height = 1,
+            Width = 7,
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.BrightRed, this.ColorScheme.Normal.Background)),
+            Text = $"({trackInformation.RecordedYear})",
+        };
+
+        this.Add(title, artist, recordingYear);
+
+        var line = new LineView(Orientation.Horizontal)
+        {
+            X = 0,
+            Y = 3
+        };
+
+        this.Add(line);
+
+        var noteringenText = "Noteringen";
+
+        this.Add(new Label
+        {
+            X = 5,
+            Y = 3,
+            Height = 1,
+            Width = noteringenText.Length + 2,
+            Text = $"\u2528{noteringenText}\u2523"
         });
 
         this.Add(new Label
         {
-            X = 0,
-            Y = 1,
+            X = 25,
+            Y = 3,
             Height = 1,
-            Width = Dim.Fill(),
-            Text = $"{trackInformation.Artist} ({trackInformation.RecordedYear})",
+            Width = 1,
+            Text = "\u2533"
         });
-        //var noteringenText = "Noteringen";
 
-        // this.LineCanvas.AddLine(new Point(0, 3), int.MaxValue, Orientation.Horizontal, LineStyle.Single);
+        var lineDown = new LineView(Orientation.Vertical)
+        {
+            X = 25,
+            Y = 4,
+        };
+        this.Add(lineDown);
 
-        //this.Add(new Label
-        //{
-        //    X = 3,
-        //    Y = 3,
-        //    Height = 1,
-        //    Width = noteringenText.Length,
-        //    Text = noteringenText
-        //});
+        var frame = new FrameView
+        {
+            X = Pos.Right(lineDown),
+            Y = 5,
+            Width = Dim.Fill(2),
+            Height = Dim.Fill(2),
+            BorderStyle = LineStyle.None
+        };
+        this.Add(frame);
+
+
+
+        var labels = new[] { "Aantal sinds onstaan", "Aantal in Top 2000", "", "Hoogste notering", "Laagste notering", "Eerste notering", "Laatste notering" };
+        var maxLenght = labels.Max(x => x.Length);
+
+        for (var i = 0; i < labels.Length; i++)
+        {
+            frame.Add(new Label
+            {
+                X = 0,
+                Y = i,
+                Text = labels[i],
+                Width = maxLenght + 1,
+            });
+        }
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 1,
+            Y = 0,
+            Width = 5,
+            Text = $"{trackInformation.Appearances}/{trackInformation.AppearancesPossible}",
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 1,
+            Y = 1,
+            Width = 5,
+            Text = $"{trackInformation.Appearances}/{trackInformation.Listings.Count}",
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 1,
+            Y = 3,
+            Width = 5,
+            Text = $"{trackInformation.Highest.Position}",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.BrightRed, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 6,
+            Y = 3,
+            Width = 7,
+            Text = $"({trackInformation.Highest.Edition})",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.Gray, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 1,
+            Y = 4,
+            Width = 5,
+            Text = $"{trackInformation.Lowest.Position}",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.BrightRed, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 6,
+            Y = 4,
+            Width = 7,
+            Text = $"({trackInformation.Lowest.Edition})",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.Gray, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 1,
+            Y = 5,
+            Width = 5,
+            Text = $"{trackInformation.First.Position}",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.BrightRed, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 6,
+            Y = 5,
+            Width = 7,
+            Text = $"({trackInformation.First.Edition})",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.Gray, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 1,
+            Y = 6,
+            Width = 5,
+            Text = $"{trackInformation.Latest.Position}",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.BrightRed, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 6,
+            Y = 6,
+            Width = 7,
+            Text = $"({trackInformation.Latest.Edition})",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.Gray, this.ColorScheme.Normal.Background)),
+        });
+
+        frame.Add(new Label
+        {
+            X = maxLenght + 6,
+            Y = 7,
+            Width = Dim.Fill(),
+            Text = $"{trackInformation.Latest.LocalUtcDateAndTime}",
+            ColorScheme = new ColorScheme(new Terminal.Gui.Attribute(Terminal.Gui.Color.Gray, this.ColorScheme.Normal.Background)),
+        });
 
         var table = new ListingInformationTableView(new ListingInformationSource(trackInformation))
         {
@@ -59,6 +218,7 @@ public class TrackInformationView : View
             Height = Dim.Fill(),
             Width = 23,
             FullRowSelect = true,
+
             Style = new TableStyle
             {
                 ShowHeaders = false,
@@ -75,25 +235,7 @@ public class TrackInformationView : View
         };
         this.Add(table);
 
-
-
-        //this.Add(new Label
-        //{
-        //    X = 0,
-        //    Y = 11,
-        //    Width = Dim.Fill(),
-        //    Height = 1,
-        //    Text = "Sinds ontstaan"
-        //});
-
-        //this.Add(new Label
-        //{
-        //    X = 0,
-        //    Y = 12,
-        //    Width = Dim.Fill(),
-        //    Height = 1,
-        //    Text = "In Top 2000"
-        //});
+        this.SetNeedsDisplay();
     }
 
 }
@@ -104,10 +246,10 @@ public class ListingInformationTableView : TableView
 
     protected override void RenderCell(Terminal.Gui.Attribute cellColor, string render, bool isPrimaryCell)
     {
-        int green = render.IndexOf("\uFC35", StringComparison.CurrentCultureIgnoreCase);
-        int yellow = render.IndexOf("\uF73A", StringComparison.CurrentCultureIgnoreCase);
-        int alsoYellow = render.IndexOf("\uF94F", StringComparison.CurrentCultureIgnoreCase);
-        int red = render.IndexOf("\uFC2C", StringComparison.CurrentCultureIgnoreCase);
+        var green = render.IndexOf("\uFC35", StringComparison.CurrentCultureIgnoreCase);
+        var yellow = render.IndexOf("\uF73A", StringComparison.CurrentCultureIgnoreCase);
+        var alsoYellow = render.IndexOf("\uF94F", StringComparison.CurrentCultureIgnoreCase);
+        var red = render.IndexOf("\uFC2C", StringComparison.CurrentCultureIgnoreCase);
 
         for (var i = 0; i < render.Length; i++)
         {
